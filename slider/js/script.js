fcdc0cb1a43661e7de3e13ff8874dbd7ed77ed40
Nata Malinova <rawr@raery.ru>
2021-11-26 01:50:19 +0700

    third slider

--- slider/js/script.js
+++ slider/js/script.js
@@ -6,7 +6,21 @@ $(document).ready(function () {
         infinite: true,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
     $('.center-items').slick({
@@ -15,6 +29,32 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 1
+    });
+
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 
